9d61111c6d6899a513f90e499365ebac1e9c422a
はるみん / halminusa32 <halminusa32@gmail.com>
2026-02-24 19:13:24 +0900

    Update sound file for move action

--- halris/solo/script.js
+++ halris/solo/script.js
@@ -354,7 +354,7 @@ tap('ctrl-rot-r', () => rotate(1)); tap('ctrl-rot-l', () => rotate(-1)); tap('ct
 // SE（効果音）管理セクション
 // ==========================================
 const SOUND_FILES = {
-    move: 'https://actions.google.com/sounds/v1/foley/drawbridge_opening.ogg', 
+    move: 'https://halminusa32.github.io/halris/solo/solian-te-n.mp3', 
     rotate: 'https://actions.google.com/sounds/v1/foley/button_click.ogg',
     clear: 'https://halminusa32.github.io/halris/solo/solian-te-n.mp3',
     tetris: 'https://halminusa32.github.io/halris/solo/solian-te-n.mp3',
