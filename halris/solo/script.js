19a7e8f501afb720731cc6baa0035fcf4529a591
はるみん / halminusa32 <halminusa32@gmail.com>
2026-02-26 08:14:46 +0900

    Update script.js

--- halris/solo/script.js
+++ halris/solo/script.js
@@ -318,7 +318,7 @@ tap('ctrl-rot-r', () => rotate(1)); tap('ctrl-rot-l', () => rotate(-1)); tap('ct
 // SE管理（Web Audio API版：コントロールセンターに出ない）
 // ==========================================
 const SOUND_FILES = {
-    move: 'https://actions.google.com/sounds/v1/foley/drawbridge_opening.ogg', 
+    move: 'https://halminusa32.github.io/halris/solo/move.mp3', 
     rotate: 'https://actions.google.com/sounds/v1/foley/button_click.ogg',
     clear: 'https://halminusa32.github.io/halris/solo/solian-te-n.mp3',
     tetris: 'https://halminusa32.github.io/halris/solo/solian-te-n.mp3',
